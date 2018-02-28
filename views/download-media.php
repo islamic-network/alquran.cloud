971774f026d70c93a16b839f2174596b85b4fb6c
Meezaan-ud-Din <meezaan@vesica.co.uk>
2018-02-28 11:29:12 +0000

    updated url

--- views/download-media.php
+++ views/download-media.php
@@ -14,7 +14,7 @@
         All the media available via the <a href="/cdn">Al Quran Cloud Content Delivery Network (CDN)</a> is available to download and use as required. Please follow the below link to the One Drive folder that contains all the media.
 		</p>
 		<p class="align-center">
-			<a href="https://onedrive.live.com/redir?resid=F6B3731D309AD56A!263160&authkey=!APDvX6cwz6DAwng&ithint=folder%2C" class="btn btn-primary" target="_blank">Download Media</a>
+			<a href="https://1drv.ms/f/s!AmrVmjAdc7P2kId4gMQNTYXWh-Nh6A" class="btn btn-primary" target="_blank">Download Media</a>
 		</p>
 		</div>
 	</div>
