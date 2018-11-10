3565fc37a356abc38070052f345f4a82bf024c84
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <meezaan@vesica.ws>
2018-11-10 10:15:03 +0000

    added build file

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## Release tag / version. If this is not for a specific release, please set this to latest, otherwise set it to a specific release.
+version=1
+
+###########################################################################################
+#UNLESS YOU WANT TO CHANGE SOMETHING TO DO WITH THE PUSH TO NEXUS, LEAVE THE BELOW ALONE #
+###########################################################################################
+## The URL of the repo. Do not change unless you're sure about this.
+prod=vesica/alquran.cloud
+
+## The actual script to build and push the image
+echo "Building production image"
+docker build -f Dockerfile . -t $prod:$version
+docker push $prod:$version
+
+if [ "$version" != "latest" ]
+    then
+        docker build -f Dockerfile . -t $prod:latest
+        docker push $prod:latest
+    fi
