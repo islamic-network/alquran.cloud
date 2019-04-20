2805188e064cc9c73b36bcb9710637f22e6f254d
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <meezaan@vesica.ws>
2019-04-20 13:13:22 +0100

    Add lb check

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,7 +1,8 @@
 <?php
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
-
+use Slim\Http\Request;
+use Slim\Http\Response;
 
 $container = $app->getContainer();
 
@@ -13,8 +14,7 @@ $container['helper'] = function($c) {
     // Create the logger
     $helper->logger = new Logger('QuranApp');
     // Now add some handlers
-    //$helper->logger->pushHandler(new StreamHandler(__DIR__.'/../logs/' . $logFile . '.log', Logger::DEBUG));
-    $helper->logger->pushHandler(new \Monolog\Handler\ErrorLogHandler());
+    $helper->logger->pushHandler(new StreamHandler('php://stdout', Logger::INFO));
 
     return $helper;
 };
@@ -37,22 +37,41 @@ $container['view'] = function ($container) {
     return new \Slim\Views\PhpRenderer($path);
 };
 
-/**
-$container['notFoundHandler'] = function ($c) {
-    return function ($request, $response) use ($c) {
-        $r = [
-        'code' => 404,
-        'status' => 'Not Found',
-        'data' => 'Invalid endpoint or resource.'
-        ];
-        $resp = json_encode($r);
-
-        return $c['response']
-            ->withStatus(404)
-            ->withHeader('Content-Type', 'application/json')
-            ->write($resp);
+$container['errorHandler'] = function ($c) {
+    return function (Request $request, Response $response, Exception $e) use ($c) {
+        $c->logger->error('Slim Error Handler Triggered', ['code' => $e->getCode(), 'message' => $e->getMessage(), 'trace' => $e->getTraceAsString()]);
+        return $c['response']->withStatus($e->getCode())
+            ->withHeader('Content-Type', 'text/html')
+            ->write($e->getMessage());
     };
-
 };
 
- */
+$container['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        return $c['response']
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'text/html')
+            ->write('Sorry, we could not find the URL you are after.');
+    };
+};
+
+/** Invoke Middleware for Load Balancer Checks */
+$app->add(function (Request $request, Response $response, $next) {
+    $lbMode = (bool) getenv('LOAD_BALANCER_MODE');
+    if ($lbMode) {
+        // Validate Key
+        if (isset($request->getHeader('X-LOAD-BALANCER')[0]) && $request->getHeader('X-LOAD-BALANCER')[0] === getenv('LOAD_BALANCER_KEY')) {
+            $response = $next($request, $response);
+
+            return $response;
+        }
+
+        throw new \Exception('Invalid Load Balancer Key.', 403);
+    }
+
+    $response = $next($request, $response);
+
+    return $response;
+});
+
+
