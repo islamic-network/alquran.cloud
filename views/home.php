11061d788a72272e60f872a8deb219b4f176c72d
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram / مِيزَانُ الْدِّينْ عَبْدُ ذِيْ الْجَلَالِ وَ الْإِكْرَامِ <meezaan@vesica.ws>
2019-09-17 08:49:57 +0100

    Update download media URI

--- views/home.php
+++ views/home.php
@@ -76,7 +76,7 @@
             Quran audio, ayah images, the database - everything is available for use.
         </p>
         <p>
-            <a href="http://downloads.alquran.cloud" target="_blank" class="btn btn-primary">Get the Media</a>
+            <a href="/download-media" target="_blank" class="btn btn-primary">Get the Media</a>
 
         </p>
     </div>
