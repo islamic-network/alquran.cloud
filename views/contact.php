612e1aac3e96a82e15f1965d64bb1b896df543dd
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-07-19 13:51:11 +0400

    Update contact page

--- views/contact.php
+++ views/contact.php
@@ -8,14 +8,27 @@
 	<div class="page-header">
         <h4>Contact</h4>
     </div>
-	<div class="row">
-		<div class="col-md-12">
-		<p>
-        If you have any questions, feedback, concerns or queries, please write to 
-&#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#105;&#115;&#108;&#097;&#109;&#105;&#099;&#046;&#110;&#101;&#116;&#119;&#111;&#114;&#107; or join the Islamic Network Discord server @ https://discord.gg/FwUy69M.
+    <div class="row">
+      <div class="col-md-12">
+        <p>
+        If you have any suggestions or would like to say something, you can:
         </p>
-		</div>
-	</div>
+
+        <ul>
+          <li>Get in touch on
+          &#115;&#117;&#112;&#112;&#111;&#114;&#116;&#064;&#105;&#115;&#108;&#097;&#109;&#105;&#099;&#046;&#110;&#101;&#116;&#119;&#111;&#114;&#107;.
+          </li>
+          <li>
+          Join and post your query on the Islamic Network Community @ <a href="https://community.islamic.network"
+          target="_blank">https://community.islamic.network</a>. This is the official supported channel for support.
+          </li>
+          <li>
+          Join the Discord server if you'd just like to chat about something related @ <a href="https://discord.gg/FwUy69M"
+          target="_blank">https://discord.gg/FwUy69M</a>.
+          </li>
+        </ul> 
+      </div>
+    </div>
 	
 
 </div>
